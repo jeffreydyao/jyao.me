99175dfd17f3cd88b9de92c85b43b00534e5b39c
jeffreydyao <z5316953@ad.unsw.edu.au>
2022-01-29 17:47:42 +1100

    Added Tailwind CSS, Inter font and favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
+
+type Props = {}
+
+class Document extends NextDocument<Props> {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default Document
