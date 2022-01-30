235909c88f51c52d2643af7385c7418e948952f5
jeffreydyao <z5316953@ad.unsw.edu.au>
2022-01-30 19:47:50 +1100

    Added key to map to fix React build error (#2)

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -38,11 +38,12 @@ export default function Projects() {
       <h2 className="text-neutral-50 text-sm md:absolute md:left-[-5.55rem] md:top-0 pb-7 font-medium">
         Projects
       </h2>
-
+      
+      <div className="flex flex-col">
       {projects.map(
-        ({ name, date, image, alt, description, builtwith, link, github }) => (
-          <div className="flex flex-col">
-            <div>
+        ({ name, date, image, alt, description, builtwith, link, github }, i) => (
+            <div key={`${i}`}>
+              <div>
               <Image
                 src={image}
                 alt={alt}
@@ -50,41 +51,44 @@ export default function Projects() {
                 height={220}
                 className="rounded-[0.875rem]"
               />
-            </div>
-
-            <h3 className="text-neutral-50 text-base md:pt-9 pt-7">{name}</h3>
-            <p className="text-neutral-400 text-sm pt-1">{date}</p>
-            <div className="flex md:flex-row flex-col justify-start pt-3 md:gap-14 gap-8">
-              <div className="flex flex-col gap-4">
-                <p className="text-neutral-300 text-sm md:max-w-xs leading-5">
-                  {description}
-                </p>
-                <p className="text-neutral-300 text-sm md:max-w-xs leading-5">
-                  {builtwith}
-                </p>
               </div>
-              <div>
-                <div className="border-t-2 w-8 border-neutral-600 md:pb-2 pb-3" />
-                <div className="flex md:flex-col flex-row align-middle md:gap-2 gap-3">
-                  <a
-                    href={link}
-                    className="text-neutral-100 text-xs md:pt-1 hover:text-blue-300 hover:cursor-pointer transition-all duration-[100]"
-                  >
-                    Check it out →
-                  </a>
-                  <a
-                    href={github}
-                    className="text-neutral-100 text-xs hover:text-blue-300 hover:cursor-pointer transition-all duration-[100]"
-                  >
-                    GitHub →
-                  </a>
-                  {/* Update with blog post once finished */}
+
+              <h3 className="text-neutral-50 text-base md:pt-9 pt-7">{name}</h3>
+              <p className="text-neutral-400 text-sm pt-1">{date}</p>
+              <div className="flex md:flex-row flex-col justify-start pt-3 md:gap-14 gap-8">
+                <div className="flex flex-col gap-4">
+                  <p className="text-neutral-300 text-sm md:max-w-xs leading-5">
+                    {description}
+                  </p>
+                  <p className="text-neutral-300 text-sm md:max-w-xs leading-5">
+                    {builtwith}
+                  </p>
+                </div>
+                <div>
+                  <div className="border-t-2 w-8 border-neutral-600 md:pb-2 pb-3" />
+                  <div className="flex md:flex-col flex-row align-middle md:gap-2 gap-3">
+                    <a
+                      href={link}
+                      className="text-neutral-100 text-xs md:pt-1 hover:text-blue-300 hover:cursor-pointer transition-all duration-[100]"
+                    >
+                      Check it out →
+                    </a>
+                    <a
+                      href={github}
+                      className="text-neutral-100 text-xs hover:text-blue-300 hover:cursor-pointer transition-all duration-[100]"
+                    >
+                      GitHub →
+                    </a>
+                    {/* Update with blog post once finished */}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          
+
         )
       )}
+      </div>
     </Block>
   );
 }
