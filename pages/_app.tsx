6e2b3bd46d5c6195090810b3ef8eda554094d64d
jeffreydyao <z5316953@ad.unsw.edu.au>
2022-02-01 00:59:31 +1100

    Removed Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,7 @@
 import "../styles/globals.css";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import * as gtag from "../lib/gtag";
 import type { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return <Component {...pageProps} />;
 };
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
-import { GA_TRACKING_ID } from "../lib/gtag";
 import React from "react";
 type Props = {};
 
@@ -8,23 +7,6 @@ class Document extends NextDocument<Props> {
     return (
       <Html lang="en">
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
           <link
             rel="preload"
             href="https://rsms.me/inter/inter.css"
