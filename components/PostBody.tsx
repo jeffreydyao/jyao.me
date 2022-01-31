351820b197666e7c9517345e4cbf31aa9ed630e1
jeffreydyao <z5316953@ad.unsw.edu.au>
2022-02-01 00:51:45 +1100

    Added post explaining how website works

--- components/PostBody.tsx
+++ components/PostBody.tsx
@@ -5,7 +5,7 @@ export default function PostBody({ content }: any) {
   return (
     <div className="md:w-[500px] max-w-[500px] pt-4">
       <article
-        className="prose prose-sm prose-neutral dark:prose-invert prose-code:before:content-none prose-code:after:content-none"
+        className="prose prose-sm prose-neutral dark:prose-invert prose-code:before:content-none prose-code:after:content-none prose-quoteless"
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,14 @@ module.exports = {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      typography: {
+        quoteless: {
+          css: {
+            'blockquote p:first-of-type::before': { content: 'none' },
+            'blockquote p:first-of-type::after': { content: 'none' },
+          }
+        }
+      }
     },
   },
   plugins: [
