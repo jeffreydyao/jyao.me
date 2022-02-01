8acc81d6ea96cb4c442311403251db914b7b0da2
jeffreydyao <z5316953@ad.unsw.edu.au>
2022-02-01 13:33:55 +1100

    Fixed back button on posts on tablet/desktop

--- components/PostTitle.tsx
+++ components/PostTitle.tsx
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }: any) {
   return (
-    <h1 className="text-neutral-50 text-lg font-semibold max-w-[500px] md:mt-[-1.2rem] pt-2">
+    <h1 className="text-neutral-50 text-lg font-semibold max-w-[500px] md:mt-[-1.2rem] md:pt-0 pt-2">
       {children}
     </h1>
   )
