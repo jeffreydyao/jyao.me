dbd4784279764069a44156917c846814b6338fa7
jeffreydyao <z5316953@ad.unsw.edu.au>
2022-02-16 23:11:14 +1100

    Fixed inconsistent padding on md: breakpoint

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,8 +5,8 @@ export default function Layout({ children }: any) {
   return (
     <>
       <Meta />
-      <div className="bg-neutral-900 min-h-screen flex flex-col items-center md:px-0 px-6 dark">
-        <div className="items-start md:mt-24 mt-8 mb-8 transition-all duration-[100]">
+      <div className="flex flex-col items-center min-h-screen px-6 bg-neutral-900 md:px-0 dark">
+        <div className="items-start md:mt-24 md:mb-24 mt-8 mb-8 transition-all duration-[100]">
           <Header />
           <main>{children}</main>
         </div>
