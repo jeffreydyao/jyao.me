00a0e91f093653bc1262e5b87c655b78435aa94f
jeffreydyao <z5316953@ad.unsw.edu.au>
2022-03-05 21:32:11 +1100

    Compressed film photos + added dynamic sitemap generation

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://jyao.me',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-};
+  siteUrl: process.env.SITE_URL || 'https://www.jyao.me',
+  generateRobotsTxt: true,
+  sitemapSize: 50000,
+}
