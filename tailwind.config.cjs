f44e75251081e1325d264054500a321d979cbf94
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2022-05-24 17:39:18 +1000

    feat: added inter/inter display

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-	presets: [require('windy-radix-palette')],
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Inter Display", fontFamily.sans],
+        sans: ["Inter", fontFamily.sans]
+      }
+    }
+  },
+  plugins: [],
+  presets: [require("windy-radix-palette")]
 }
