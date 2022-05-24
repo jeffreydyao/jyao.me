f07f874ed9ab77060853a7b4517f19c5ee60e12c
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2022-05-24 15:54:55 +1000

    feat: added windy-radix-palette

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind({
+    // Use custom base.css to apply windy-radix-palette directive
+    config: {applyBaseStyles: false}
+  })]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,4 +4,5 @@ module.exports = {
 		extend: {},
 	},
 	plugins: [],
+	presets: [require('windy-radix-palette')],
 }
