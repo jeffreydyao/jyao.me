b8111368b435f3fb9882bb8ac872190d3580442f
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2022-05-25 14:14:41 +1000

    feat: base head component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), tailwind({
-    // Use custom base.css to apply windy-radix-palette directive
-    config: {applyBaseStyles: false}
-  })]
-});
+  site: "https://jyao.me",
+  integrations: [
+    sitemap(),
+    tailwind({
+      // Use custom base.css to apply windy-radix-palette directive
+      config: { applyBaseStyles: false }
+    })
+  ]
+})
