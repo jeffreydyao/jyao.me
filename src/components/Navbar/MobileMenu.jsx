264cca5e05ff369915bac9d593408ccb7aac6504
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2022-05-27 12:22:42 +1000

    feat: mobile menu unmount/mount transitions

--- src/components/Navbar/MobileMenu.jsx
+++ src/components/Navbar/MobileMenu.jsx
@@ -1,5 +1,6 @@
-import { createEffect, createSignal, onCleanup } from "solid-js";
+import { createEffect, createSignal, onCleanup, Show } from "solid-js";
 import { animate, stagger } from "motion";
+import { Motion, Presence } from "@motionone/solid";
 
 export default function MobileMenu() {
   const [isMenuOpen, setIsMenuOpen] = createSignal(false);
@@ -13,7 +14,7 @@ export default function MobileMenu() {
   }
 
   function Separator() {
-    return <div class="w-full border-t border-gray-6" />;
+    return <div class="w-full border-t border-gray-7" />;
   }
 
   createEffect(() => {
@@ -22,15 +23,11 @@ export default function MobileMenu() {
       const titles = menu.querySelectorAll("li");
       const separators = menu.querySelectorAll("div");
 
-      animate(titles, { opacity: [0, 1] }, { duration: 0.415, delay: stagger(0.115) });
-      animate(separators, { width: [0, 1] }, { duration: 0.465, delay: stagger(0.16) });
-      console.log("Menu is open!");
+      animate(titles, { opacity: [0, "100%"] }, { duration: 0.415, delay: stagger(0.115) });
+      animate(separators, { width: [0, "100%"] }, { duration: 0.465, delay: stagger(0.16) });
     }
   });
 
-  // TODO: Refactor to following when Motion One for Solid fixed:
-  // 1. Move menu out into another component
-  // 2. Add Presence wrapped to animate unmount/mount into DOM
   return (
     <>
       <button
@@ -42,39 +39,39 @@ export default function MobileMenu() {
           <div class="tham-inner" />
         </div>
       </button>
-      {isMenuOpen() && (
-        <ul
-          id="menu-root"
-          class="absolute top-0 left-0 w-screen h-screen text-md font-medium pt-24 px-8 bg-gray-3 z-10 flex flex-col gap-4"
-        >
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <Separator />
-          <li>
-            <a href="/blog">Blog</a>
-          </li>
-          <Separator />
-          <li>
-            <a href="/projects">Projects</a>
-          </li>
-          <Separator />
-          <li>
-            <a href="/now">Now</a>
-          </li>
-          <Separator />
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <Separator />
-        </ul>
-      )}
+      <Presence>
+        <Show when={isMenuOpen()}>
+          <Motion.ul
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.155 }}
+            id="menu-root"
+            class="absolute top-0 left-0 w-screen h-screen text-md font-medium pt-24 px-8 bg-gray-3 z-10 flex flex-col gap-4"
+          >
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <Separator />
+            <li>
+              <a href="/blog">Blog</a>
+            </li>
+            <Separator />
+            <li>
+              <a href="/projects">Projects</a>
+            </li>
+            <Separator />
+            <li>
+              <a href="/now">Now</a>
+            </li>
+            <Separator />
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <Separator />
+          </Motion.ul>
+        </Show>
+      </Presence>
     </>
-
-    // Store open closed signal
-
-    // When signal open, render menu
-
-    // When signal closed, close menu
   );
 }
