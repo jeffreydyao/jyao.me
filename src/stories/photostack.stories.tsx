9c13085cfe512924ff69dcb1e819078f4e334f0c
Jeffrey Yao <jeffrey.yao@dijgtal.com>
2022-08-16 10:40:03 +1000

    feat: add new webp images to photostack

--- src/stories/photostack.stories.tsx
+++ src/stories/photostack.stories.tsx
@@ -1,17 +1,24 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment, @next/next/no-img-element */
 import type { Story } from "@ladle/react";
 import "tailwindcss/tailwind.css";
+import PhotoStack1 from "../img/photostack1.webp";
+import PhotoStack2 from "../img/photostack2.webp";
+import PhotoStack3 from "../img/photostack3.webp";
 
 // Create story for photo stack
 export const PhotoStack: Story = () => (
   <div className="relative">
     <div className="overflow-hidden border-8 border-gray-50 drop-shadow-md rounded-3xl w-60 h-80">
-      <img src="https://images.unsplash.com/photo-1512641406448-6574e777bec6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
+      {/* @ts-ignore */}
+      <img src={PhotoStack1} alt="" />
     </div>
     <div className="absolute overflow-hidden border-8 rotate-[3deg] top-4 left-5 -z-10 border-gray-50 drop-shadow-lg rounded-3xl w-60 h-80">
-      <img src="https://images.unsplash.com/photo-1512641406448-6574e777bec6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
+      {/* @ts-ignore */}
+      <img src={PhotoStack2} alt="" />
     </div>
     <div className="absolute overflow-hidden border-8 border-gray-100 -z-20 top-8 left-11 rotate-6 drop-shadow-xl rounded-3xl w-60 h-80">
-      <img src="https://images.unsplash.com/photo-1512641406448-6574e777bec6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
+      {/* @ts-ignore */}
+      <img src={PhotoStack3} alt="" />
     </div>
   </div>
 );
