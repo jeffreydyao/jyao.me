23104ff4645b19d77b7124102cef5c0656775d0f
Jeffrey Yao <hello@jyao.me>
2022-12-29 13:08:49 +1000

    feat: configure astro w/ @image, tailwind, inter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), image(
+    // https://docs.astro.build/en/guides/integrations-guide/image/#installing-sharp-optional
+    {
+      serviceEntryPoint: '@astrojs/image/sharp'
+    }
+  )]
+});
